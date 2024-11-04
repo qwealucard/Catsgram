14dd8f99f4310a525e24fc5c40a3167ad187fc47
Артём <qwealucard@yandex.ru>
2024-11-04 12:57:16 +0300

    Catsgram

---
+++ src/main/java/ru/yandex/practicum/catsgram/CatsgramApp.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.catsgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsgramApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CatsgramApp.class, args);
+    }
+} 
---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/HomeController.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String homePage() {
+        return "<h1>Приветствуем вас, в приложении Котограм<h1>";
+    }
+}
